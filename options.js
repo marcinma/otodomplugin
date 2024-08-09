d5026bd2ea6abc1e4814f4e72ca14ad6b4411149
Marcin Makowski <marcin.makowski@spglobal.com>
2024-08-09 15:11:15 +0200

    introduce dynamic colors

--- options.js
+++ options.js
@@ -1,13 +1,15 @@
-// Saves options to chrome.storage
 const saveOptions = () => {
     const addedDate = document.getElementById('addedDate').checked;
     const modDate = document.getElementById('modDate').checked;
     const days = document.getElementById('days').checked;
-  
+    const maxDays = document.getElementById('maxDays').value;
+    const position = document.getElementById('position').value;
+    if(maxDays<1){
+        maxDays=1;
+    }
     chrome.storage.sync.set(
-      { addedDate: addedDate, modDate: modDate, days: days },
+      { addedDate: addedDate, modDate: modDate, days: days, maxDays: maxDays, position: position },
       () => {
-        // Update status to let user know options were saved.
         const status = document.getElementById('status');
         status.textContent = 'Options saved.';
         setTimeout(() => {
@@ -17,15 +19,16 @@ const saveOptions = () => {
     );
   };
   
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
+
   const restoreOptions = () => {
     chrome.storage.sync.get(
-      { addedDate: true, modDate: true, days: true },
+      { addedDate: true, modDate: true, days: true, maxDays:120, position:'br' },
       (items) => {
         document.getElementById('addedDate').checked = items.addedDate;
         document.getElementById('modDate').checked = items.modDate;
         document.getElementById('days').checked = items.days;
+        document.getElementById('maxDays').value = items.maxDays;
+        document.getElementById('position').value = items.position;
       }
     );
   };
